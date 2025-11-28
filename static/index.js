037038169abdfd2e0abc988d14bf74f0cd74d725
Milijan Mosic <milijan.mosic@gmail.com>
2025-11-28 17:52:03 +0100

    Fixed background image handling

--- static/index.js
+++ static/index.js
@@ -2,7 +2,6 @@ import LiquidBackground from "https://cdn.jsdelivr.net/npm/threejs-components@0.
 
 const app = LiquidBackground(document.getElementById("canvas"));
 
-app.loadImage("https://images.unsplash.com/photo-1618764117597-0626010bf40f");
 // app.loadImage("https://assets.codepen.io/33787/liquid.webp");
 // app.liquidPlane.material.metalness = 0.75;
 // app.liquidPlane.material.roughness = 0.25;
