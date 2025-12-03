9db9dad417835ca40721f9ca0b67e1c45165a1aa
milijan-mosic <milijan.mosic@gmail.com>
2025-12-03 21:17:48 +0100

    Disabled rain effects

--- static/index.js
+++ static/index.js
@@ -1,17 +1,17 @@
-import LiquidBackground from "https://cdn.jsdelivr.net/npm/threejs-components@0.0.22/build/backgrounds/liquid1.min.js";
+// import LiquidBackground from "https://cdn.jsdelivr.net/npm/threejs-components@0.0.22/build/backgrounds/liquid1.min.js";
 
-const app = LiquidBackground(document.getElementById("canvas"));
+// const app = LiquidBackground(document.getElementById("canvas"));
 
-// app.loadImage("https://assets.codepen.io/33787/liquid.webp");
+// app.loadImage("/static/background.jpg");
 // app.liquidPlane.material.metalness = 0.75;
 // app.liquidPlane.material.roughness = 0.25;
 // app.liquidPlane.uniforms.displacementScale.value = 5;
 // app.setRain(true);
 
-app.liquidPlane.material.metalness = 0.0;
-app.liquidPlane.material.roughness = 0.0;
-app.liquidPlane.uniforms.displacementScale.value = 0;
-app.setRain(false);
+// app.liquidPlane.material.metalness = 0.0;
+// app.liquidPlane.material.roughness = 0.0;
+// app.liquidPlane.uniforms.displacementScale.value = 0;
+// app.setRain(false);
 
 // ----------------------------------------------------------------
 
