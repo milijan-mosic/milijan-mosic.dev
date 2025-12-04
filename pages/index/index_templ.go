5a0560152131fe0c4a37143d9e248bf88b39bcfe
milijan-mosic <milijan.mosic@gmail.com>
2025-12-04 01:21:55 +0100

    Disabled Pricing section

--- pages/index/index_templ.go
+++ pages/index/index_templ.go
@@ -69,10 +69,6 @@ func Index() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = Pricing().Render(ctx, templ_7745c5c3_Buffer)
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
 		templ_7745c5c3_Err = CTA().Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
