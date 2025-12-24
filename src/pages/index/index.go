aaa1e8547828a31250ad22f94557289aac38a2e2
Milijan Mosic <milijan.mosic@gmail.com>
2025-12-24 19:25:04 +0100

    Added HSTS policy

--- src/pages/index/index.go
+++ src/pages/index/index.go
@@ -6,6 +6,10 @@ import (
 
 func Homepage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set(
+		"Strict-Transport-Security",
+		"max-age=63072000; includeSubDomains; preload",
+	)
 
 	err := Index().Render(r.Context(), w)
 	if err != nil {
