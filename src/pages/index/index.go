f4b28809f8c2701c4f2329344e1b5eec7eb755c7
Milijan Mosic <milijan.mosic@gmail.com>
2025-12-25 15:55:09 +0100

    Fixed serving of files

--- src/pages/index/index.go
+++ src/pages/index/index.go
@@ -6,22 +6,22 @@ import (
 
 func Homepage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	w.Header().Set(
-		"Strict-Transport-Security",
-		"max-age=63072000; includeSubDomains; preload",
-	)
-	w.Header().Set(
-		"Cross-Origin-Opener-Policy",
-		"same-origin",
-	)
-	w.Header().Set(
-		"X-Frame-Options",
-		"SAMEORIGIN",
-	)
-	w.Header().Set(
-		"Content-Security-Policy",
-		"frame-ancestors 'self'",
-	)
+	// w.Header().Set(
+	// 	"Strict-Transport-Security",
+	// 	"max-age=63072000; includeSubDomains; preload",
+	// )
+	// w.Header().Set(
+	// 	"Cross-Origin-Opener-Policy",
+	// 	"same-origin",
+	// )
+	// w.Header().Set(
+	// 	"X-Frame-Options",
+	// 	"SAMEORIGIN",
+	// )
+	// w.Header().Set(
+	// 	"Content-Security-Policy",
+	// 	"frame-ancestors 'self'",
+	// )
 
 	err := Index().Render(r.Context(), w)
 	if err != nil {
